2427170452e7dadade1ebf7d0abd6e586e79b8fe
14600-netizen <14600@phanthong.ac.th>
2026-08-02 14:53:24 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,109 @@
+// ===========================
 
+// Fade In Animation
+
+// ===========================
+
+const fadeElements = document.querySelectorAll(
+
+".card,.timeline-item,.profile-box,.hero,.skill"
+
+);
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+fadeElements.forEach(el=>{
+
+el.classList.add("fade");
+
+observer.observe(el);
+
+});
+
+// ===========================
+
+// Popup Image
+
+// ===========================
+
+const popup=document.getElementById("popup");
+
+const popupImg=document.getElementById("popup-img");
+
+const images=document.querySelectorAll(".card img");
+
+if(images.length>0){
+
+images.forEach(img=>{
+
+img.addEventListener("click",()=>{
+
+popup.style.display="flex";
+
+popupImg.src=img.src;
+
+});
+
+});
+
+}
+
+function closePopup(){
+
+popup.style.display="none";
+
+}
+
+// ===========================
+
+// Back To Top
+
+// ===========================
+
+const topBtn=document.createElement("button");
+
+topBtn.id="topBtn";
+
+topBtn.innerHTML="↑";
+
+document.body.appendChild(topBtn);
+
+window.onscroll=function(){
+
+if(document.documentElement.scrollTop>250){
+
+topBtn.style.display="block";
+
+}
+
+else{
+
+topBtn.style.display="none";
+
+}
+
+}
+
+topBtn.onclick=function(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+}
